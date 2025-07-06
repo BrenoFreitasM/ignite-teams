4bef075838ad76c3e1755c7533734435a1134e95
Breno Freitas <brenofreitasmendes@gmail.com>
2025-07-06 19:24:53 -0300

    bugfix: adjusting theme path on app.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components";
-import theme from "@theme/index";
+import theme from "./src/theme/index";
 
 export default function App() {
   return (
